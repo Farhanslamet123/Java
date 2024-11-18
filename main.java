59be193a7838de36048e80a52ceaead52393ad39
Farhanslamet123 <slametfarhan916@gmail.com>
2024-11-18 08:31:07 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+class java_length{
+  public static void main (String[] args){
+    String nama1 = "Farhan";
+    String nama2 = "Slamet ";
+    
+    System.out.println("Panjang nama 1 : " + nama1.length());
+    System.out.println("Panjang nama 2 : " + nama2.length());
+  }
+}
